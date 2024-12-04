8779d1006b50f24a6e658783cfe2b19fd9e375f9
Menghua Xiao <menxiao@microsoft.com>
2024-12-04 15:08:48 +0800

    initial web project

---
+++ src/main/java/me/menxiao/study/azd/AzdTemplatePlayApplication.java
@@ -0,0 +1,13 @@
+package me.menxiao.study.azd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzdTemplatePlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzdTemplatePlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/menxiao/study/azd/AzdTemplatePlayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.menxiao.study.azd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzdTemplatePlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
